e127246e98e569b5c77755a097b3cde59a4298de
Jilin Liu <jilin@unrivaled.com>
2017-12-13 19:22:03 -0800

    add custom UIController extension

--- FriendsListDemoApp/CustomExtensions.swift
+++ FriendsListDemoApp/CustomExtensions.swift
@@ -1,1 +1,19 @@
+//
+//  CustomExtensions.swift
+//  FriendsListDemoApp
+//
+//  Created by Jilin Liu on 12/13/17.
+//  Copyright © 2017 Jilin Liu. All rights reserved.
+//
 
+import Foundation
+import UIKit
+
+extension ViewController {
+    func displayAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
